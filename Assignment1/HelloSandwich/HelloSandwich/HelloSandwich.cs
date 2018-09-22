c1fc1b7db3d16e888c4c7db783d03db08d198a31
unknown <nematoesandnematodes@gmail.com>
2018-09-22 16:38:50 -0700

    Added completed HelloSandwich project, added local .gitignore

---
+++ Assignment1/HelloSandwich/HelloSandwich/HelloSandwich.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * HelloSandwich.cs
+ * Created by Brian Bos
+ */
+
+class HelloSandwich
+{
+    static void Main()
+    {
+        string inputMouseCount;
+        string inputName;
+
+        Console.WriteLine("What is your name?");
+        inputName = Console.ReadLine();
+
+        Console.Write("\nHow many mice does it take to make a turkey sandwich?\nAnswer: ");
+        inputMouseCount = Console.ReadLine(); // Numeric input is encouraged, but not required
+
+        Console.Write($"\nEntered name  : {inputName}\nEntered answer: {inputMouseCount}\n\n\"{inputName}\" believes that it takes {inputMouseCount} mice to make a turkey sandwich!\n\n");
+    }
+}
