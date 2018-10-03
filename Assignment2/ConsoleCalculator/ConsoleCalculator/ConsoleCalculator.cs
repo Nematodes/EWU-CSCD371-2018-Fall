bef596031a8ce19793bd17ece66988bdf81ceca0
unknown <nematoesandnematodes@gmail.com>
2018-10-03 01:00:59 -0700

    Completed basic calculator functionality, added a few (not completely comprehensive) unit tests

--- Assignment2/ConsoleCalculator/ConsoleCalculator/ConsoleCalculator.cs
+++ Assignment2/ConsoleCalculator/ConsoleCalculator/ConsoleCalculator.cs
@@ -11,13 +11,17 @@ using System.Threading.Tasks;
 
 namespace BrianBosAssignment2Namespace
 {
-    class ConsoleCalculator
+    public class ConsoleCalculator
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            int firstOperand;
             bool isCharacterValid;
+            bool isErrorThrown;
             bool isUserInputValid;
+            int secondOperand;
             string userInput;
+            int userInputStringPosition;
             char[] validCharacters = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '.', ' ' };
 
             Console.WriteLine($"ConsoleCalculator is now running.{Environment.NewLine}Enter \"Bye\" to quit the program.{Environment.NewLine}");
@@ -60,10 +64,115 @@ namespace BrianBosAssignment2Namespace
                     continue;
                 }
 
-                // userInput = userInput.Replace(" ", "");
+                userInput = userInput.Replace(" ", "");
 
-                Console.WriteLine($"Result: Over 9000!{Environment.NewLine}");
+                firstOperand = getOperand(userInput, 0, out userInputStringPosition);
+
+                if (userInputStringPosition == -1)
+                {
+                    continue;
+                }
+
+                int operatorStringPosition = userInputStringPosition;
+                secondOperand = getOperand(userInput, userInputStringPosition + 1, out userInputStringPosition);
+
+                if (userInputStringPosition == -1)
+                {
+                    continue;
+                }
+
+                int equationResult = 0;
+
+                if (userInputStringPosition != -1)
+                {
+                    checked
+                    {
+                        try
+                        {
+                            isErrorThrown = false;
+
+                            switch (userInput[operatorStringPosition])
+                            {
+                                case '+':
+                                    equationResult = firstOperand + secondOperand;
+                                    break;
+
+                                case '-':
+                                    equationResult = firstOperand - secondOperand;
+                                    break;
+
+                                case '*':
+                                    equationResult = firstOperand * secondOperand;
+                                    break;
+
+                                case '/':
+                                    equationResult = firstOperand / secondOperand;
+                                    break;
+
+                                default:
+                                    Console.WriteLine($"Error: Invalid operator \"{userInput[operatorStringPosition]}\" encountered");
+                                    break;
+                            }
+                        }
+                        catch (DivideByZeroException)
+                        {
+                            Console.WriteLine($"Error: Division by zero{Environment.NewLine}");
+                            isErrorThrown = true;
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine($"Error: Integer overflow{Environment.NewLine}");
+                            isErrorThrown = true;
+                        }
+                    }
+
+                    if (!isErrorThrown)
+                    {
+                        Console.WriteLine($"Result: {equationResult}{Environment.NewLine}");
+                    }
+                }
             }
+        }
+
+        static int getOperand(string equationString, int startIndex, out int operandEndStringPosition)
+        {
+            int stringPosition = startIndex;
+            int tempOperand = 0;
+
+            if (stringPosition >= equationString.Length)
+            {
+                Console.WriteLine($"Error: stringPosition was greater than or equal to equationString.Length in method getOperand(){Environment.NewLine}");
+                operandEndStringPosition = -1;
+                return 0;
+            }
+
+            while (equationString[stringPosition] != '+' && equationString[stringPosition] != '-' && equationString[stringPosition] != '*' && equationString[stringPosition] != '/')
+            {
+                checked
+                {
+                    try
+                    {
+                        tempOperand *= 10;
+                        tempOperand += Convert.ToInt32(equationString[stringPosition].ToString());
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine($"Error: An operand was larger than {int.MaxValue} (that is, int.MaxValue){Environment.NewLine}");
+                        operandEndStringPosition = -1;
+                        return 0;
+                    }
+                }
+
+                stringPosition++;
+
+                if (stringPosition >= equationString.Length)
+                {
+                    break;
+                }
+            }
+
+            operandEndStringPosition = stringPosition;
+            return tempOperand;
         }
     }
 }
--- Assignment2/ConsoleCalculator/ConsoleCalculatorUnitTester/UnitTests.cs
+++ Assignment2/ConsoleCalculator/ConsoleCalculatorUnitTester/UnitTests.cs
@@ -3,11 +3,32 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BrianBosAssignment2Namespace
 {
     [TestClass]
-    public class UnitTests
+    public class ArithmeticTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddition()
         {
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect("ConsoleCalculator is now running.\nEnter \"Bye\" to quit the program.\n\nEnter an expression to evaluate: <<1 + 2\n>>Result: 3\n\nEnter an expression to evaluate: <<Bye\n", ConsoleCalculator.Main);
+        }
+
+        [TestMethod]
+        public void TestSubtraction()
+        {
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect("ConsoleCalculator is now running.\nEnter \"Bye\" to quit the program.\n\nEnter an expression to evaluate: <<123 - 456\n>>Result: -333\n\nEnter an expression to evaluate: <<Bye\n", ConsoleCalculator.Main);
+        }
+
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect("ConsoleCalculator is now running.\nEnter \"Bye\" to quit the program.\n\nEnter an expression to evaluate: <<360 * 420\n>>Result: 151200\n\nEnter an expression to evaluate: <<Bye\n", ConsoleCalculator.Main);
+        }
+
+        [TestMethod]
+        public void TestDivision()
+        {
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect("ConsoleCalculator is now running.\nEnter \"Bye\" to quit the program.\n\nEnter an expression to evaluate: <<108 / 12\n>>Result: 9\n\nEnter an expression to evaluate: <<Bye\n", ConsoleCalculator.Main);
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect("ConsoleCalculator is now running.\nEnter \"Bye\" to quit the program.\n\nEnter an expression to evaluate: <<5 / 2\n>>Result: 2\n\nEnter an expression to evaluate: <<Bye\n", ConsoleCalculator.Main);
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect("ConsoleCalculator is now running.\nEnter \"Bye\" to quit the program.\n\nEnter an expression to evaluate: <<1 / 0\n>>Error: Division by zero\n\nEnter an expression to evaluate: <<Bye\n", ConsoleCalculator.Main);
         }
     }
 }
